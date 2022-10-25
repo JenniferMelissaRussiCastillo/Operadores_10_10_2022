7d2a8871bec5a483c21073fa0308f049f72366b2
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-25 02:01:57 -0500

    Operadores

--- api.php
+++ api.php
@@ -1,7 +1,63 @@
 <?php 
-header('Access-Control-Allow-Origin: *');
-$num1 =1;
-$num2 = 2;
-$res = $num1 + $num2;
-echo "Soy el servisor ".$_SERVER['HTTP_HOST']." gracias por conectarme :)";
+    header('Access-Control-Allow-Origin: *');
+    header("Content-Type: application/json;charset=utf-8");
+
+    $res = new stdClass();
+    $num1 =55.78;
+    $num2 = 2;
+    
+    //operardor suma 
+    $suma = $num1 + $num2;
+    $res->suma= $suma;
+
+    //operador resta
+    $resta = $num1- $num2;
+    $res->resta=$resta;
+
+    //operador dividir
+    $division = $num1 / $num2;
+    $res-> division=$division;
+
+    //operario multiplicar
+    $multiplicar = $num1 * $num2;
+    $res-> multiplicar =$multiplicar;
+
+    //-------------------------------------------------------------------
+    // operardor potencia (elevar)
+    $potencia = $num1 ** $num2+1;
+    $res->potencia = $potencia;
+
+    // operador modulo (resto)
+    $modulo = $num1 % $num2;
+    $res->modulo =$modulo;
+    //---------------------------------------------------------------------
+
+    // operador de raiz 
+    $raizCuadrada = sqrt($num1);
+    $res->raiz= $raizCuadrada;
+
+   $elevar = $num1 **3;
+   $resta = $num1 - $elevar;
+   $suma= $elevar + $resta;
+   $res->operarion =$suma; 
+    
+   // operador raiz cubica
+   $raizCubica = pow($num1,(1/3));
+   $res->raizCubica=$raizCubica;
+
+
+   //operador numero aleatorio
+   $aleatorio = rand(1,$num1);
+   $res-> aleatorio = $aleatorio;
+
+   // operador numero aleatorio redondeo
+   $redondear= round($num1,1);
+   $res->redondear= $redondear;
+
+   
+
+    echo json_encode($res, JSON_PRETTY_PRINT);
+
+
+    
 ?> 
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 addEventListener("DOMContentLoaded", async()=>{
     let peticion = await fetch("https://jennifermelissarussicastillo.000webhostapp.com/Operadores_07_10_2022/api.php");
     let data = await peticion.text();
-    document.body.innerHTML = `<h1 style="background: red; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}</h1>`;
+    document.body.innerHTML = `<h1 style="background: blue; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}`;
+    
 })
