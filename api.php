52a465913f3be483e81e62af0146d0f9875b3e40
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-25 00:25:08 -0500

    Operadores_07_10_2022

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+$num1 =1;
+$num2 = 2;
+$res = $num1 + $num2;
+echo "Soy el servisor ".$_SERVER['HTTP_HOST']." gracias por conectarme :)";
+?> 
---
+++ main.js
@@ -0,0 +1,5 @@
+addEventListener("DOMContentLoaded", async()=>{
+    let peticion = await fetch("https://jennifermelissarussicastillo.000webhostapp.com/Operadores_07_10_2022/api.php");
+    let data = await peticion.text();
+    document.body.innerHTML = `<h1 style="background: red; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}</h1>`;
+})
