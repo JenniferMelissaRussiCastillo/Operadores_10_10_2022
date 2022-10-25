8965afb2360b4c0774581d00ada7b693e760ae27
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-25 03:17:53 -0500

    Operadores final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 addEventListener("DOMContentLoaded", async()=>{
     let peticion = await fetch("https://jennifermelissarussicastillo.000webhostapp.com/Operadores_07_10_2022/api.php");
     let data = await peticion.text();
-    document.body.innerHTML = `<h1 style="background: blue; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}`;
-    
+   // document.body.innerHTML = `<h1 style="background: blue; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}`;
+    document.body.querySelector("#datos").innerHTML=data;
 })
