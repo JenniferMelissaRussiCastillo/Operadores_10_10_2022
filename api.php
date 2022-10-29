46f23798917fadfeae24890f25d9b714c305761d
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-28 22:02:01 -0500

    Formulario_Ope

--- api.php
+++ api.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
     header('Access-Control-Allow-Origin: *');
-    header("Content-Type: application/json;charset=utf-8");
+    header('Content-Type: application/json; charset=utf-8');
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $_DATA["servidor"] = $_SERVER["HTTP_HOST"];
+    extract($_DATA);
 
-    $res = new stdClass();
-    
-   echo "Soy el servisor formularios JSON".$_SERVER['HTTP_HOST']." gracias por conectarme :)";
-    
-?> 
+    echo json_encode($_DATA, JSON_PRETTY_PRINT);
+
+?>
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let form= document.querySelector("#Mydata");
-    form.addEventListener("submit", (e)=>{
+    let form = document.querySelector("#MyData");
+    form.addEventListener("submit", async(e)=>{
         e.preventDefault();
-        let data = Object.fromEntries(new FormData(e.targer));
-        console.log(data);
+        let json = JSON.stringify(Object.fromEntries(new FormData(e.target)));
+        let config = {
+            method: form.method,
+            body: json
+        };
+        let peticion = await fetch(form.action, config);
+        let data = await peticion.text();
+        document.querySelector("pre").innerHTML = data;
     })
-
 })
